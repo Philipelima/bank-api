8b34b5a232bc95e63c14d042a39915caa209b909
philipelima <philipematheus.lim@gmail.com>
2025-04-02 19:25:15 -0300

    fix: rename TransactionType.php to TransactionStatus.php

--- app/Enums/Transaction/TransactionType.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Enums\Transaction;
-
-enum TransactionType: string {
-    case PENDING   = 'pending';
-    case COMPLETED = 'completed';
-    case FAILED    = 'failed';
-    case CANCELED  = 'canceled';
-}
