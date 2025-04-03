8fa7fd44b07a6c94f2090f488b5001c0389214d8
philipelima <philipematheus.lim@gmail.com>
2025-04-03 14:46:30 -0300

    feat(auth): ensure user type  middleware

---
+++ app/Http/Middleware/EnsureUserType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $type): Response
+    {
+        if ($request->user()->tokenCan($type)) {
+            return $next($request);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 403);
+    }
+}
