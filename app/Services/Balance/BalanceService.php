f321785a4f1a1ef67a561653f5a7496096dfd134
philipelima <philipematheus.lim@gmail.com>
2025-04-03 18:35:13 -0300

    chore: removing unused dependencies

--- app/Services/Balance/BalanceService.php
+++ app/Services/Balance/BalanceService.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 namespace App\Services\Balance;
 
 use App\Models\User;
-use App\Enums\User\UserType;
 
-
-use App\Exceptions\User\InvalidUserTypeException;
 use App\Models\Operation\Balance;
 use App\Repository\Operation\Balance\BalanceHistoryRepository;
-use Illuminate\Support\Facades\DB;
+
 
 class BalanceService 
 {
