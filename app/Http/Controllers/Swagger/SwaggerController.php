649ca23cdb3576b20e234f782572f5915e8523b0
philipelima <philipematheus.lim@gmail.com>
2025-04-02 21:10:54 -0300

    feat: Swagger Controller

---
+++ app/Http/Controllers/Swagger/SwaggerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * @OA\Info(
+ *      title="Simple Bank",
+ *      version="1.0",
+ *      description="Api de Banco Simples",
+ *      @OA\Contact(
+ *          email="seuemail@exemplo.com"
+ *      ),
+ * )
+ */
+class SwaggerController extends Controller
+{
+}
