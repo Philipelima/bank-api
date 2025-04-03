1792e3b7a946337e7bb59abf907b24dde110da81
philipelima <philipematheus.lim@gmail.com>
2025-04-03 14:49:20 -0300

    chore(auth): add middlewares to app config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'user.type' => \App\Http\Middleware\EnsureUserType::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
