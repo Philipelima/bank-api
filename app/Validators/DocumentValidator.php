9f4d86c43d535746e814be822b37354ad499c2e1
philipelima <philipematheus.lim@gmail.com>
2025-04-03 12:52:25 -0300

    feat: refactoring to follow phpmd recommendations

--- app/Validators/DocumentValidator.php
+++ app/Validators/DocumentValidator.php
@@ -5,41 +5,48 @@ namespace App\Validators;
 
 class DocumentValidator
 {
-    public static function isValidCpf(string $cpf): bool
+    private string $document;
+
+    public function setDocument(string $document)
     {
-        $cpf = preg_replace('/[^0-9]/', '', $cpf); // Remove caracteres não numéricos
+        $this->document = $document;
+    }
+
+    public function isValidCpf(): bool
+    {
+        $cpf = preg_replace('/[^0-9]/', '', $this->document); // Remove caracteres não numéricos
         if (strlen($cpf) !== 11 || preg_match('/(\d)\1{10}/', $cpf)) {
             return false;
         }
         for ($t = 9; $t < 11; $t++) {
-            $d = 0;
+            $digit = 0;
             for ($c = 0; $c < $t; $c++) {
-                $d += $cpf[$c] * (($t + 1) - $c);
+                $digit += $cpf[$c] * (($t + 1) - $c);
             }
-            $d = ((10 * $d) % 11) % 10;
-            if ($cpf[$c] != $d) {
+            $digit = ((10 * $digit) % 11) % 10;
+            if ($cpf[$c] != $digit) {
                 return false;
             }
         }
         return true;
     }
 
-    public static function isValidCnpj(string $cnpj): bool
+    public function isValidCnpj(): bool
     {
-        $cnpj = preg_replace('/[^0-9]/', '', $cnpj);
+        $cnpj = preg_replace('/[^0-9]/', '', $this->document);
         if (strlen($cnpj) !== 14) {
             return false;
         }
-        $multiplicadores = [
+        $multipliers = [
             5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 6, 5, 4, 3, 2
         ];
         for ($t = 12; $t < 14; $t++) {
-            $d = 0;
+            $digit = 0;
             for ($c = 0; $c < $t; $c++) {
-                $d += $cnpj[$c] * $multiplicadores[$c + (14 - $t)];
+                $digit += $cnpj[$c] * $multipliers[$c + (14 - $t)];
             }
-            $d = ((10 * $d) % 11) % 10;
-            if ($cnpj[$c] != $d) {
+            $digit = ((10 * $digit) % 11) % 10;
+            if ($cnpj[$c] != $digit) {
                 return false;
             }
         }
