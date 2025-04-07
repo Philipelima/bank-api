52561ff20c07028509120bd8ab3d5ee5573fa4d1
philipelima <philipematheus.lim@gmail.com>
2025-04-07 20:45:30 -0300

    feat(transfer): add updateBalance function into BalanceService

--- app/Services/Balance/BalanceService.php
+++ app/Services/Balance/BalanceService.php
@@ -21,4 +21,26 @@ class BalanceService
         return $this->balanceHistoryRepository->last($user->uuid);
     }
 
+    public function updateBalance(User $user, float $amount, string $type) 
+    {
+        $lastOperation   = $this->balanceHistoryRepository->last($user->uuid);
+        $previousBalance = $lastOperation?->balance ?? 0.00;
+
+        $newBalance =  match ($type) {
+
+            'deposit'  => $previousBalance + $amount,
+            'transfeer', 'withdraw' => $previousBalance - $amount,
+            
+            default => throw new \InvalidArgumentException("Invalid operation type: {$type}")
+        };
+
+        return $this->balanceHistoryRepository->create([
+            'user_uuid'      => $user->uuid,
+            'balance'        => $newBalance,
+            'last_balance'   => $previousBalance,
+            'change_amount'  => $amount,
+            'operation_type' => $type,
+        ]);
+    } 
+
 }
