54435f8c8402748e431c3a629abf960d37ab1a9a
philipelima <philipematheus.lim@gmail.com>
2025-04-07 18:55:11 -0300

    feat(transfer): TransferRepository

---
+++ app/Repository/Operation/Transfer/TransferRepository.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository\Operation\Transfer;
+
+use App\Models\Operation\Balance;
+use App\Models\Operation\Transfer\Transfer;
+
+class TransferRepository 
+{
+    public function __construct(
+        private Transfer $transfer
+    ){   
+    }
+
+    public function create(array $data): Transfer
+    {
+        return $this->transfer->create($data);
+    }
+
+    
+}
