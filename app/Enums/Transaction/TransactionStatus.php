fb8779b69715ae9aae44ab83f0a5941050dd1399
philipelima <philipematheus.lim@gmail.com>
2025-04-07 22:20:55 -0300

    remove(transfer): remove old transfer exceptions

--- app/Enums/Transaction/TransactionStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Enums\Transaction;
-
-enum TransactionStatus: string {
-    case PENDING   = 'pending';
-    case COMPLETED = 'completed';
-    case FAILED    = 'failed';
-    case CANCELED  = 'canceled';
-}
--- app/Enums/Transaction/TransactionType.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-namespace App\Enums\Transaction;
-
-enum TransactionType: string {
-    case PERSON_TO_BUSINESS = 'P2B';
-    case PERSON_TO_PERSON   = 'P2P';
-}
